685e76b703132c17f84087eb5a2e351bc3721aa8
Christian Sonne <christian@filmhub.com>
2024-10-28 20:11:32 +0100

    only call into wasm when it's ready

--- www/public/ffprobe-worker.js
+++ www/public/ffprobe-worker.js
@@ -1,4 +1,14 @@
-onmessage = (message) => {
+const ffprobeModulePromise = new Promise((resolve, reject) => {
+  try {
+    self.importScripts('ffprobe-wasm.js')
+    Module.onRuntimeInitialized = () => resolve(Module)
+  } catch (error) {
+    console.log('ffprobe failed to load "ffprobe-wasm.js"')
+    reject(new Error("Failed to load Wasm module"))
+  }
+})
+
+onmessage = async (message) => {
   const [type, file] = message.data
   const [origin] = message.ports
 
@@ -11,8 +21,10 @@ onmessage = (message) => {
     FS.mount(WORKERFS, { files: [file] }, '/work')
 
     try {
+      // wait for module to be ready
+      const ffprobeModule = await ffprobeModulePromise
       // Call the wasm module.
-      const { name, error, ...info } = Module.get_file_info(`/work/${file.name}`)
+      const { name, error, ...info } = ffprobeModule.get_file_info(`/work/${file.name}`)
 
       // Remap streams into collection.
       const streams = []
@@ -48,9 +60,7 @@ onmessage = (message) => {
         },
       })
     } catch (unknownError) {
-      // for yet to be explained reasons, accessing error here crashes the worker
-      // send back generic error for now
-      origin.postMessage({ error: 'ffprobe crashed' })
+      origin.postMessage({ error: `ffprobe crashed: ${unknownError.stack}` })
     }
 
     // Cleanup mount.
@@ -61,11 +71,4 @@ onmessage = (message) => {
   default:
     break
   }
-}
-try {
-  self.importScripts('ffprobe-wasm.js') // Load ffprobe into worker context.
-} catch (error) {
-  // we don't have access to browserLogger inside the worker
-  // eslint-disable-next-line no-console
-  console.log('ffprobe failed to load "ffprobe-wasm.js"')
 }
